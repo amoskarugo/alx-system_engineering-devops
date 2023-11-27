0b160c68623a08209939762f919f4b2d7fc0157e
amoskarugo <karugoamos@gmail.com>
2023-11-27 16:35:49 +0300

    use puppet to configure ssh config file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,14 +2,14 @@
 # using puppet to make changes to the default ssh config file
 include stdlib
 
-file { 'Disable passwd auth':
+file_line { 'Disable passwd auth':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '    PasswordAuthentication no',
   replace => true,
 }
 
-file { 'Set identity file':
+file_line { 'Set identity file':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => '     IdentityFile ~/.ssh/school',
